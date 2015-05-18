9f04c2a81ebeef372c3c29b54acb8a2cb2b2fbef
Andrew Patton <andrew@acusti.ca>
2015-05-17 21:58:49 -0400

    :toilet: Remove unused build process scaffolding

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
+/*globals require */
 var gulp = require('turris-gulp-tasks')([
     'serve',
     'build',
     'debug',
     'test',
 ], require('./buildConfig.js'));
-// load custom tasks
-require('./gulp/custom-task.js')(gulp);
 
 gulp.task('default', ['debug', 'serve']);
 gulp.task('deploy', ['build']);
