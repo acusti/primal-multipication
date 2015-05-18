d9638bfd86ad48afde801706b5f256111f5da728
Andrew Patton <andrew@acusti.ca>
2015-05-18 01:26:44 -0400

    Reorganize and extend number iterator tests

--- test/number-iterator.js
+++ test/number-iterator.js
@@ -1,19 +1,26 @@
 import numbers from '../src/helpers/number-iterator.js';
 
 describe('Number iterator: ', function() {
-	it('Returns an iterator for numbers with a working next method', function() {
-		let iterator = numbers(2)[Symbol.iterator]();
-		// Starts at 0
+	it('Returns an iterator that generates a sequence of numbers with length equal to its first parameter', function() {
+		const iterator = numbers(2)[Symbol.iterator]();
+		// Starts at 0 with increment defaulting to 1
 		iterator.next().value.should.equal(0);
 		iterator.next().value.should.equal(1);
 		// Length is 2, so iterator is now done
 		iterator.next().done.should.equal(true);
+	});
 
+	it('Accepts custom increment number as second parameter', function() {
 		// Increment by 3
-		iterator = numbers(2, 3)[Symbol.iterator]();
+		let iterator = numbers(2, 3)[Symbol.iterator]();
 		iterator.next();
 		iterator.next().value.should.equal(3);
+		// Negative increment
+		iterator = numbers(2, -11)[Symbol.iterator]();
+		iterator.next();
+		iterator.next().value.should.equal(-11);
 	});
+
 	it('When used in array spread, returns a sequential array of numbers', function() {
 		[...numbers(5)].should.eql([0, 1, 2, 3, 4]);
 	});
