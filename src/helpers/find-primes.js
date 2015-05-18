eae6fc74cf147b68ff30a0f30e6fd1ab053ce657
Andrew Patton <andrew@acusti.ca>
2015-05-18 12:43:11 -0400

    Add isPrime helper function (tests :white_check_mark:)

--- src/helpers/find-primes.js
+++ src/helpers/find-primes.js
@@ -8,4 +8,24 @@ function findPrimes(total) {
 	return primes;
 }
 
+export function isPrime(number) {
+	if (number < 2) {
+		return false;
+	}
+	// Quick check for even numbers
+	if (number !== 2 && number % 2 === 0) {
+		return false;
+	}
+	// We start checking at 3, factor can be maximum 1/3 the number, and we will only check odd numbers
+	// Number of factors to check is number / 3 (max 1/3) - 3 (for offset of start: 3) / 2 (odd only)
+	const length = Math.round((number / 3 - 2) / 2);
+	if (length <= 0) {
+		return true;
+	}
+	// Check all possible odd factors
+	return [ ...numbers({start : 3, increment : 2, length})].every(function(possibleFactor) {
+		return number % possibleFactor !== 0;
+	});
+}
+
 export default findPrimes;
