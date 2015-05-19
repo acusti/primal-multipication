d9140f2d0fff59033c34e707d0dabeba18d45a3c
Andrew Patton <andrew@acusti.ca>
2015-05-19 14:35:46 -0400

    :racehorse: Faster prime iteration (using 6n ± 1 rule)
    Implements basic wheel factorization (for n = 2 * 3) to narrow down
    field of potential primes and prime factors that are checked. Reduces
    running time of findPrimes(30000, true) test from 188407ms to 1005ms.

--- src/helpers/find-primes.js
+++ src/helpers/find-primes.js
@@ -4,21 +4,55 @@ import numbers from './number-iterator.js';
 // let cachedPrimes = [];
 
 function findPrimes(length, isSingle = false) {
-	let start;
+	const increment = 6;
+	let current = 0;
+	let isNMinusOne = false;
 	let primes = [ ...numbers({ length }) ].map(() => {
-		if (start === undefined) {
-			// Initialize start at 3 (next pass)
-			start = 3;
-			// Initialize primes at 2
-			return 2;
+		// Exceptions for 2 and 3
+		if (current < increment) {
+			if (!current) {
+				// Increment current to make it not falsy
+				current++;
+				// First prime is 2
+				return 2;
+			}
+			// Kick off proper counting at 6
+			current = increment;
+			// Next prime is 3
+			return 3;
 		}
-		let prime = start;
-		while (!isPrime(prime)) {
-			prime += 2;
+		let multiple = current;
+		let prime    = getPrimes(multiple);
+		while (!prime) {
+			multiple += increment;
+			prime = getPrimes(multiple);
 		}
-		start = prime + 2;
+		current = multiple;
+		// If we are ready to check the next multiple of 6
+		if (!isNMinusOne) {
+			current += increment;
+		}
 		return prime;
 	});
+
+	function getPrimes(multiple) {
+		let prime = false;
+		if (!isNMinusOne) {
+			isNMinusOne = true;
+			if (isPrime(multiple - 1)) {
+				return multiple - 1;
+			}
+		}
+		// Either 6n - 1 just got checked, or it was checked in a previous pass
+		if (isNMinusOne) {
+			isNMinusOne = false;
+			if (isPrime(multiple + 1)) {
+				return multiple + 1;
+			}
+		}
+		return prime;
+	}
+
 	return isSingle ? primes[primes.length - 1] : primes;
 }
 
@@ -26,19 +60,34 @@ export function isPrime(number) {
 	if (number < 2) {
 		return false;
 	}
-	// Quick check for even numbers
-	if (number !== 2 && number % 2 === 0) {
+	if (number === 2 || number === 3) {
+		return true;
+	}
+	// Check for multiples of 2 and 3
+	if (number % 2 === 0 || number % 3 === 0) {
 		return false;
 	}
-	// We start checking at 3, factor can be maximum 1/3 the number, and we will only check odd numbers
-	// Number of factors to check is number / 3 (max 1/3) - 3 (for offset of start: 3) / 2 (odd only)
-	const length = Math.round((number / 3 - 2) / 2);
+	// Outside of 2 and 3, all primes take form 6n - 1 or 6n + 1 (where n is any integer)
+	// So if number - 1 && number + 1 don't have remainders when divided by 6, can't be prime
+	if ((number - 1) % 6 !== 0 && (number + 1) % 6 !== 0) {
+		return false;
+	}
+	const limit = Math.sqrt(number);
+	// If the sqrt is an integer (no remainder when divided by 1), it’s not prime
+	if (limit % 1 === 0) {
+		return false;
+	}
+	// If it passes those tests, manually verify that it's prime (like 65, 121, 1037, etc.)
+	// We only need to check factors of form 6n + or - 1 up to the square root of the number
+	// Maximum number of checks needed is (sqrt(number) + 1 [the plus part of + or - 1]) / 6 * 2
+	// And all that - 1 because we're starting from 6, not 0
+	const length = Math.round((limit + 1) / 3) - 1;
 	if (length <= 0) {
 		return true;
 	}
-	// Check all possible odd factors
-	return [ ...numbers({start : 3, increment : 2, length})].every(function(possibleFactor) {
-		return number % possibleFactor !== 0;
+	// Check all possible prime factors
+	return [ ...numbers({start : 6, increment : 6, length})].every(function(possibleFactor) {
+		return number % (possibleFactor - 1) !== 0 && number % (possibleFactor + 1) !== 0;
 	});
 }
 
