0bd470aad4fcf969ea00dd8f6dd2cb8da675c879
Andrew Patton <andrew@acusti.ca>
2015-05-21 18:23:50 -0400

    :memo: Revise parameter help text

--- src/components/Parameters.jsx
+++ src/components/Parameters.jsx
@@ -13,7 +13,7 @@ class Parameters extends React.Component {
                     How many primes do you want to show? <NumberEditor min={1} max={200000} step={1} decimals={0} {...this.props} />
                 </label>
                 <p className="parameters__help">
-                    <span className="glyphicon glyphicon-question-sign"></span> To edit the total: click on the number and drag, click and use the arrow keys, or double click to enter a new number
+                    <span className="glyphicon glyphicon-question-sign"></span> To edit the total: click the number and drag, use keyboard up and down arrows, or double click to enter a new number
                 </p>
             </div>
         );
