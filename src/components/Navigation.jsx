6102f3f7f9c37284455282c12e01e9738a8803f0
Andrew Patton <andrew@acusti.ca>
2015-06-01 13:24:11 -0400

    import icon into Navigation, use it in View Source link

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
-// react
 import React from 'react';
 import ReactRouter from 'react-router';
 import basePath from '../helpers/base-path.js';
+import IconGithub from './assets/IconGithub.jsx';
 
 class Navigation extends React.Component {
     constructor(props) {
@@ -34,6 +34,7 @@ class Navigation extends React.Component {
                         <ul className="nav navbar-nav">
                             <li><ReactRouter.Link to={basePath}>Home</ReactRouter.Link></li>
                             <li><ReactRouter.Link to={`${basePath}about`}>About</ReactRouter.Link></li>
+                            <li><a href="https://github.com/acusti/primal-multiplication" className="navbar-nav__github-link"><IconGithub /> View Source</a></li>
                         </ul>
                     </div>
                 </div>
